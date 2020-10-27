dc25c46103e05c9b8f7a14938e5f25e9c11172bf
r00t0k <artists2@naver.com>
2020-10-27 19:35:29 +0900

    동영상 재생 전까지 구현

--- MacEclassProject_main.py
+++ MacEclassProject_main.py
@@ -36,17 +36,20 @@ TD = date.today()
 def playVideo():
     pass
 
+def switchIframe():
+    driver.switch_to.frame("tool_content")
+    return
 
-
-
-
+def EndswitchIframe():
+    driver.switch_to.default_content()
+    return
 
 while(1):
     print('------동작 목록------')
 
     print('1. 수업 목록 보기')
-    print('2. 밀린 동영상 강의 시청')
-    print('3. 해야할 과제 출력')
+    print('2. 밀린 동영상 강의 시청 (들을 수 있는 수업은 전부 자동 시청합니다.)')
+    print('3. 해야할 과제 출력\n\n')
 
     Select_Num = int(input("동작을 선택하세요(-1 입력시 종료) : "))
     print(Select_Num)
@@ -68,22 +71,51 @@ while(1):
         for count in range(0, Clist_Num): # 과목개수 만큼 돈다.
             Clist = driver.find_elements_by_class_name('ic-DashboardCard') #ic-DashboardCard
             Clist[count].click()
+            print("이 수업은 ", driver.find_elements_by_class_name("ellipsible")[1].text, "수업 입니다.")
             try:
                 driver.find_element_by_class_name('context_external_tool_3').click()
-                try:
-                    #iframe switch
-                    driver.switch_to.frame("tool_content")
+                switchIframe()
 
+                try:
                     driver.find_element_by_class_name('xn-common-btn-unfold-icon').click()
                     print("강의를 펼쳤습니다.")
-
-                    #iframe Endswitch
-                    driver.switch_to.default_content()
                 except:
                     print("강의가 이미 펼쳐져 있습니다.")
+
+                # 행동 입력
+                EndswitchIframe()
+                switchIframe()
+                time.sleep(1.5)
+
+
+
+                ClassNotTakenA = (driver.find_elements_by_css_selector("div.xnci-description-component-type-icon.learn.unpublished.start.movie")) # 법정교육 같은 수업들 카운트
+                ClassNotTakenB = (driver.find_elements_by_css_selector("div.xnci-description-component-type-icon.learn.unpublished.start.everlec")) # 법정교육말고 다른 수업들 카운트
+                print("들을수 있는 수업이 ", (len(ClassNotTakenA + ClassNotTakenB)), "개 있습니다.\n")
+
+
+                ClassNotTakenNameList = ClassNotTakenA + ClassNotTakenB
+
+                for ClassNotTake in ClassNotTakenNameList:
+                    ClassNotTake.click()
+                '''
+                for i in classNameList:
+                    print(i.text)
+                    i.click()
+                classNameList = []
+                #print(classNameList)
+                '''
+                #print(element_count)
+                #print("video count: " + len(element_count))
+
+
+                EndswitchIframe()
+                driver.back()
+
             except:
-                print(driver.find_element_by_class_name('ellipsible').text + ' 수업은 수업 콘텐츠가 없습니다.')
-            driver.find_element_by_class_name('menu-item-icon-container').click() # 돌아가기(공통된 대쉬보드 값 찾기)
+                print(driver.find_elements_by_class_name('ellipsible')[1].text + ' 수업은 수업 콘텐츠가 없습니다.')
+            driver.back()
+            #driver.find_element_by_id('global_nav_dashboard_link').click() # 돌아가기(공통된 대쉬보드 값 찾기)
 
 
     elif Select_Num == 3:
